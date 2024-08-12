34c8b4a221fe38ecb58a99698c30611edd2374f7
SereSant <sere.santurion12@gmail.com>
2024-08-11 23:12:11 -0300

    Eventos, div  y addEventListener

---
+++ eventos.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function(){
+    let div = document.getElementById("div")
+    div.addEventListener("click", function(){
+       alert("Hola!Soy el div")
+    })
+})
+
+
